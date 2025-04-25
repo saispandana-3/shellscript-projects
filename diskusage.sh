a7303f8613fad4ff5613f69a18539019b14e204b
saispandana-3 <saispandanamaddireddy3@gmail.com>
2025-04-25 16:59:34 -0400

    eleven

--- diskusage.sh
+++ diskusage.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
- df -h | grep '^/dev/sda1' | awk 'BEGIN { print "Filesystem      Available" } { print $1 "      " $4 }' >> disk.txt
+df -h | grep '^/dev/sda1' | awk 'BEGIN { print "Filesystem      Available" } { print $1 "      " $4 }'
+available_space=$(df -h | grep '^/dev/sda1' | awk '{print $4}' | sed 's/[A-Za-z]//g')
+if [ $available_space -ge 1000 ]
+then
+echo "high disk usage
+else
+echo "perfect"
+
 
