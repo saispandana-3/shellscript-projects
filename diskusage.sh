b26b8402f5a670a16687d632a70a786cf4b89e72
saispandana-3 <saispandanamaddireddy3@gmail.com>
2025-04-25 17:05:43 -0400

    fourteen

--- diskusage.sh
+++ diskusage.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 df -h | grep '^/dev/sda1' | awk 'BEGIN { print "Filesystem      Available" } { print $1 "      " $4 }'
 available_space=$(df -h | grep '^/dev/sda1' | awk '{print $4}' | sed 's/[A-Za-z]//g')
-if [ $available_space -ge 1000 ]
+if [ $available_space -ge 500 ]
 then
-echo "high disk usage"
+echo "Hello, this is a test email" | mail -s "high disk usage" saispandana33@gmail.com
 else
 echo "perfect"
 fi
