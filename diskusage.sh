f50127d58372877bcc84d5aad2297bae17dd94d4
saispandana-3 <saispandanamaddireddy3@gmail.com>
2025-04-25 17:02:09 -0400

    thirteen

--- diskusage.sh
+++ diskusage.sh
@@ -3,7 +3,7 @@ df -h | grep '^/dev/sda1' | awk 'BEGIN { print "Filesystem      Available" } { p
 available_space=$(df -h | grep '^/dev/sda1' | awk '{print $4}' | sed 's/[A-Za-z]//g')
 if [ $available_space -ge 1000 ]
 then
-echo "high disk usage
+echo "high disk usage"
 else
 echo "perfect"
 fi
